6c7aca54e9efdf7aba8e56341f1091242abb0852
Lima Zurmati <urzalalima@gmail.com>
2024-09-13 19:07:07 +0000

    Create Profile Class Model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Profile model, related to User
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    github = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    image = models.ImageField(
+    upload_to='images/', default='../default_profile_te45n4'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+
