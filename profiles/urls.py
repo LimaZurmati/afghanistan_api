f4efcd1fdb3e210b36fa9f6223824a5665df2174
Lima Zurmati <urzalalima@gmail.com>
2024-09-15 18:29:29 +0000

    Create a new file permissions.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
     path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
