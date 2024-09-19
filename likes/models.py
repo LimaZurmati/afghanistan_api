7f82d52890be308e56e70bcc9c0b7485fc190c2c
Lima Zurmati <urzalalima@gmail.com>
2024-09-19 18:07:27 +0000

    Created class Meta.

--- likes/models.py
+++ likes/models.py
@@ -4,12 +4,18 @@ from posts.models import Post
 
 class Like(models.Model):
     """
-    Like model, related to 'owner' and 'post'.
-    'owner' is a User instance and 'post' is a Post instance.
-    'unique_together' makes sure a user can't like the same post twice.
+    Like model, related to use post.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Post, related_name='likes', on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+
+    
+
+
