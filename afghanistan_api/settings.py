e541fc736dd1044e01e1863edc7c4a9dcffe4ad8
Lima Zurmati <urzalalima@gmail.com>
2024-10-17 18:52:36 +0000

    Add new secret-key

--- afghanistan_api/settings.py
+++ afghanistan_api/settings.py
@@ -47,6 +47,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'afghanistan_api.serializers.CurrentUserSerializer'
@@ -56,14 +57,12 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4@866993s2sufqaxehyh3k&94q#)c#3do(l$8f8fx+0ubaoj5y'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-limazurmati-afghanistan-y9u7bktt9hi.ws.codeinstitute-ide.net',
-    'localhost',
-    '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', 'afghanistan_api.herokuapp.com']
 
 # CSRF settings
 CSRF_TRUSTED_ORIGINS = [
@@ -91,6 +90,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',  # Optional, if using social authentication
     'dj_rest_auth.registration',
+    'corsheaders',
     'followers',
     'profiles',
     'posts',
@@ -102,6 +102,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -112,6 +113,16 @@ MIDDLEWARE = [
     'django.contrib.sites.middleware.CurrentSiteMiddleware',  # Required if using sites framework
     # 'allauth.account.middleware.AccountMiddleware',  # Required for allauth
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+ else:
+     CORS_ALLOWED_ORIGIN_REGEXES = [
+         r"^https://.*\.gitpod\.io$",
+     ]
+ CORS_ALLOW_CREDENTIALS = True     
 
 ROOT_URLCONF = 'afghanistan_api.urls'
 
@@ -148,7 +159,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-print('coneccted')
+
 
 
 # Password validation
