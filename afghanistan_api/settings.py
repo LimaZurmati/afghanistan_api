d823d4ace6fe62e8b3d298dc143fe90b6c8a80d1
Lima Zurmati <urzalalima@gmail.com>
2024-10-17 19:02:46 +0000

    Correct indentation

--- afghanistan_api/settings.py
+++ afghanistan_api/settings.py
@@ -118,11 +118,11 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
- else:
+else:
      CORS_ALLOWED_ORIGIN_REGEXES = [
          r"^https://.*\.gitpod\.io$",
      ]
- CORS_ALLOW_CREDENTIALS = True     
+CORS_ALLOW_CREDENTIALS = True     
 
 ROOT_URLCONF = 'afghanistan_api.urls'
 
