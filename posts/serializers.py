19bd760b91d0f55b0a19097ac03c40805e20d56e
Lima Zurmati <urzalalima@gmail.com>
2024-11-06 19:56:17 +0000

    upadte serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -12,17 +12,19 @@ class PostSerializer(serializers.ModelSerializer):
     comments_count = serializers.ReadOnlyField()
     is_public = serializers.BooleanField(default=True)  # Include is_public
 
+    # Validate image uploads
     def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
+        if value.size > 2 * 1024 * 1024:  # Limit to 2MB
             raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
+        if value.height > 4096:  # Limit height
             raise serializers.ValidationError('Image height larger than 4096px!')
-        if value.image.width > 4096:
+        if value.width > 4096:  # Limit width
             raise serializers.ValidationError('Image width larger than 4096px!')
         return value
 
+    # Validate video uploads
     def validate_video(self, value):
-        if value.size > 10 * 1024 * 1024:  # Example limit of 10MB for video
+        if value.size > 10 * 1024 * 1024:  # Limit to 10MB
             raise serializers.ValidationError('Video size larger than 10MB!')
         return value
 
@@ -43,6 +45,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'video',  # Added video field
-            'image_filter', 'like_id', 'likes_count',
-            'comments_count', 'is_public',
+            'like_id', 'likes_count', 'comments_count', 'is_public',
         ]
