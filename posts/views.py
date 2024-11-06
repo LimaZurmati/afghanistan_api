106118e0efe6028d7e9007a9a5258dbbbb308ad3
Lima Zurmati <urzalalima@gmail.com>
2024-11-06 19:52:01 +0000

    Add  perform_create

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from afghanistan_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+import cloudinary
+import cloudinary.uploader
 
 class PostList(generics.ListCreateAPIView):
     """
@@ -44,7 +46,28 @@ class PostList(generics.ListCreateAPIView):
     ]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        request = self.request
+        
+        # Initialize placeholder for media URLs
+        image_url = None
+        video_url = None
+
+        # Check if there's an image file in the request
+        if 'image' in request.FILES:
+            image_file = request.FILES['image']
+            # Upload the image to Cloudinary
+            cloudinary_response = cloudinary.uploader.upload(image_file, resource_type='image')
+            image_url = cloudinary_response.get('secure_url', None)
+
+        # Check if there's a video file in the request
+        if 'video' in request.FILES:
+            video_file = request.FILES['video']
+            # Upload the video to Cloudinary
+            cloudinary_response = cloudinary.uploader.upload(video_file, resource_type='video')
+            video_url = cloudinary_response.get('secure_url', None)
+
+        # Save the post with the media URLs
+        serializer.save(owner=request.user, image=image_url, video=video_url)
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
